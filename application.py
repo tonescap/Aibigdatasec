ea075f335ceb6eb31bc9c52dd88bd2cf24abdbf5
ddoGas <ddogas101@gmail.com>
2021-11-18 02:31:05 +0900

    Update application.py

--- application.py
+++ application.py
@@ -20,19 +20,22 @@ def normalize(document):
             document[j] = '*'
     return ('').join(document)
 
-def main(filepath, checkpoint):
-    
-    with open(filepath, 'r') as f:
-        document = f.read()
+def main(folder_path, checkpoint, output):
+    documents = []
+    for filename in os.listdir(folder_path):
+        with open(os.path.join(folder_path, filename),mode='r') as f:
+            document = f.read()
+        document = normalize(document)
+        sentence = re.sub("[^A-Za-z\*\$\-]+", ' ', document).lower().split()
+        documents.append(sentence)
 
     with open('Temp.pkl', 'rb') as handle:
         datas = pickle.load(handle)
 
-    document = normalize(document)
-    sentence = re.sub("[^A-Za-z\*\$\-]+", ' ', document).lower().split()
-    n = torch.zeros(2000, dtype = torch.int64)
-    onehot = [datas[word] for word in sentence if word in datas][:2000]
-    n[:len(onehot)] = torch.LongTensor(onehot)
+    n = torch.zeros([len(documents), 2000], dtype = torch.int64)
+    for idx, sentence in enumerate(documents):
+        onehot = [datas[word] for word in sentence if word in datas][:2000]
+        n[idx, :len(onehot)] = torch.LongTensor(onehot)
 
     device = 'cpu'
     model = TokenCNN()
@@ -41,20 +44,23 @@ def main(filepath, checkpoint):
     model = model.to(device)
 
     model.eval()
-    out = model(n.unsqueeze(0))
-    if out>=0:
-        print('Malicious')
-    else:
-        print('Benign')
+    f = open(output,mode='w')
+    for i in range(n.shape[0]):
+        out = model(n[i].unsqueeze(0))
+        if out>=0:
+            f.write('Malicious\n')
+        else:
+            f.write('Benign\n')
+    f.close()
 
 if __name__ == '__main__':
     # argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--filename', required=True)
+    parser.add_argument('-i', '--input_dir', required=True)
     parser.add_argument('-c', '--checkpoint', required=True)
+    parser.add_argument('-o', '--output_path', required = True)
     args = parser.parse_args()
-    print(args)
 
-    main(args.filename, args.checkpoint)
+    main(args.input_dir, args.checkpoint, args.output_path)
 
 #'Classifier/data/train/decode5/e7b28ee5-ee7d-4ce9-8e80-42bb5ce07971'
